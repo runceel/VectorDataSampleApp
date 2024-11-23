3328364e76e425eb2587dcad5a771985cf3aa13f
Kazuki Ota <kaota@microsoft.com>
2024-11-23 23:12:46 +0900

    add ollama and qdrant

--- VectorDataSampleApp.AppHost/Program.cs
+++ VectorDataSampleApp.AppHost/Program.cs
@@ -1,3 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// ollama を追加してデータの永続化と、コンテナがデバッグ終了時に落ちないようにする
+var ollama = builder.AddOllama("ollama")
+    .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent);
+// ベクトル用のモデルを追加
+var embeddingModel = ollama.AddModel("bge-large");
+
+// qdrant を追加してデータの永続化と、コンテナがデバッグ終了時に落ちないようにする
+var qdrant = builder.AddQdrant("qdrant")
+    .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent);
+
 builder.Build().Run();
